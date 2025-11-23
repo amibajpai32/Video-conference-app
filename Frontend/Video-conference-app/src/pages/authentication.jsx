20cec639341820eeeeb5e9caafa9cbdc38eeed5b
amibajpai32 <amithebest6@gmaol.com>
2025-11-23 20:56:49 +0530

    First commit

---
+++ Frontend/Video-conference-app/src/pages/authentication.jsx
@@ -0,0 +1,186 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Snackbar from '@mui/material/Snackbar';
+import { AuthContext } from '../contexts/AuthContext';
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const defaultTheme = createTheme();
+
+export default function Authentication() {
+
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [name, setName] = React.useState("");
+  const [error, SetError] = React.useState("");
+  const [message, setMessage] = React.useState("");
+
+  const [formState, setFormState] = React.useState(0); // 0 → Login, 1 → Register
+  const [open, setOpen] = React.useState(false);
+
+  const { handleRegister, handleLogin } = React.useContext(AuthContext);
+
+  const handleAuth = async () => {
+    try {
+
+      if (formState === 1) {
+        // REGISTER
+        let result = await handleRegister(name, username, password);
+        console.log(result);
+        setUsername("");
+        setMessage(result);
+        setOpen(true);
+        SetError("")
+        setFormState(0)
+        setPassword("")
+
+      }
+
+      if (formState === 0) {
+        // LOGIN
+        let result = await handleLogin(username, password);
+        setMessage("Login successful");
+        setOpen(true);
+      }
+
+    } catch (err) {
+      return;
+      let message = (err.response.data.message);
+
+      SetError(message);
+    }
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+
+        {/* LEFT IMAGE */}
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+
+        {/* RIGHT FORM CARD */}
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+
+            <div style={{ marginBottom: "15px" }}>
+              <button
+                style={{ marginRight: "10px" }}
+                onClick={() => setFormState(0)}
+              >
+                Signin
+              </button>
+              <button onClick={() => setFormState(1)}>
+                Signup
+              </button>
+            </div>
+
+            {/* FORM */}
+            <Box component="form" noValidate sx={{ mt: 1 }}>
+
+              {/* ONLY SHOW FULL NAME IN SIGNUP */}
+              {formState === 1 && (
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  label="Full Name"
+                  name="username"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              )}
+
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                label="Username"
+                name="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                label="Password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+
+              {/* ERROR MESSAGE */}
+              <p style={{ color: "red" }}>{error}</p>
+
+              <Button
+                type="button"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                onClick={handleAuth}
+              >
+                {formState === 0 ? "Login" : "Register"}
+              </Button>
+
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+
+          </Box>
+        </Grid>
+      </Grid>
+
+      <Snackbar
+        open={open}
+        autoHideDuration={4000}
+        message={message}
+      />
+    </ThemeProvider>
+  );
+}
